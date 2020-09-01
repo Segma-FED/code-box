b0e63ea01ad6d2ef0b508c4a10240326ba4579ad
Lucas <825816425@qq.com>
2020-09-01 11:02:32 +0800

    init

---
+++ config/plugin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/** @type Egg.EggPlugin */
+module.exports = {
+  // had enabled by egg
+  // static: {
+  //   enable: true,
+  // }
+  enable: true,
+  package: 'egg-view-vue',
+};
