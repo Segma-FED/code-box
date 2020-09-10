77d446160ffd43fd976280c18ffd73a72adf62cc
Lucas <825816425@qq.com>
2020-09-10 11:55:50 +0800

    feat(功能添加): 应用容器化；用node运行构建命令

--- app/service/watch.js
+++ app/service/watch.js
@@ -4,20 +4,51 @@
  */
 'use strict';
 const Service = require('egg').Service;
-let lastModifyTime = 0;
 const chokidar = require('chokidar');
 const path = require('path');
+const { exec } = require('child_process');
+let lastModifyTime = 0;
+// 需要监听变化的文件路径
+const watchPath = path.resolve('/usr/src/node-app/watch-folder');
+const runBuildPath = path.resolve('/usr/src/node-app/run-build-folder');
+const buildScript = './node_modules/.bin/vue-cli-service build --no-clean';
 
 class WatchService extends Service {
+  /**
+   * 监听文件变化
+   * */
   async startWatchFile() {
     lastModifyTime = new Date().getTime();
-    chokidar.watch(path.resolve('/home/project/segma-framework/src'), {
+    const { ctx } = this;
+    chokidar.watch(watchPath, {
       ignored: /node_modules/,
     })
-      .on('all', (event, path) => {
+      .on('all', async (event, path) => {
         console.log(event, path);
-        lastModifyTime = new Date().getTime();
+        const runSuccess = await ctx.service.watch.runBuild();
+        if (runSuccess) {
+          lastModifyTime = new Date().getTime();
+        }
       });
+  }
+
+  /**
+   * 运行构建
+   * */
+  async runBuild() {
+    try {
+      exec(buildScript, { cwd: runBuildPath }, (err, stdout) => {
+        if (err) {
+          console.error(err);
+          return false;
+        }
+        console.log(stdout);
+        return true;
+      });
+    } catch (e) {
+      console.log('runBuildError:', e);
+      return false;
+    }
   }
 
   async getLastModifyTime() {
